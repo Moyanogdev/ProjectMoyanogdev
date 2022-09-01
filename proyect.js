705e60d3238d3d1ee9a5a646a8eb0679d6350ef4
Gonzalo Moyano <gmoyanodvlpr@gmail.com>
2022-09-01 15:59:26 -0300

    Portfolio Moyano 4. Change

--- proyect.js
+++ proyect.js
@@ -6,16 +6,14 @@ let mountain_front = document.getElementById('mountain_two');
 let header = document.querySelector('header');
 
 let stars_two = document.getElementById('stars_footer');
-let planet_two = document.getElementById('planet_footer');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    stars.style.left = value * 0.25 + 'px';
+    stars.style.left = value * 0.15 + 'px';
     planet.style.top = value * 1.05 + 'px';
     mountain_behind.style.top = value * 0.5 + 'px';
     mountain_front.style.top = value * 0 + 'px';
     header.style.top = value * 0.8 + 'px';
 
-    stars_two.style.left = value * 0.15 + 'px';
-    planet_two.style.left = value * 0.08 + 'px';
+    stars_two.style.left = value * 0.20 + 'px';
 })
