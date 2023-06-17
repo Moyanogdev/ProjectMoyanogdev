61be301205e0dc0477f8c96853d861027cd81c84
Moyano Gonzalo Federico <gmoyanodvlpr@gmail.com>
2023-06-16 23:36:55 -0300

    newContact

--- js/proyect.js
+++ js/proyect.js
@@ -24,3 +24,73 @@ window.addEventListener('scroll', function(){
 
     stars_two.style.left = value * 0.20 + 'px';
 })
+
+// NAVIGATION BAR EFECTS | RESPONSIVE
+
+let sideMenu = document.getElementById("sideMenu");
+
+function openmenu(){
+    sideMenu.style.right = "0";
+}
+
+function closemenu(){
+    sideMenu.style.right = "-200px";
+}
+
+// LANGUAGE OPTION
+
+let check=document.querySelector(".check");
+check.addEventListener('click', language);
+
+function language(){
+    let id=check.checked;
+    if(id == true){
+        location.href="es/index.html";
+    } else{
+        location.href="../index.html";
+    }
+}
+
+// SMOOTH SCROLL
+
+let scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1000,
+	speedAsDuration: true
+});
+
+// EMAIL JS 
+
+const btn = document.getElementById('button');
+const msgConfirm = document.getElementById("msgConfirm");
+
+document.getElementById('form')
+.addEventListener('submit', function(event) {
+event.preventDefault();
+
+btn.value = ' Sending... ';
+
+const serviceID = 'default_service';
+const templateID = 'template_51an4h7';
+
+emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+        msgConfirm.innerHTML = "Message sent succesfully"
+        setTimeout(function(){
+            msgConfirm.innerHTML = ""
+        },5000)
+        form.reset()
+    }, (err) => {
+        btn.value = 'Send Message';
+        alert(JSON.stringify(err));
+        });
+});
+
+// SCROLL UP EFECTS
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scrollUp')
+    this.scrollY >= 1500 ? scrollUp.classList.add('showScroll')
+                        : scrollUp.classList.remove('showScroll')
+}
+
+window.addEventListener('scroll', scrollUp);
